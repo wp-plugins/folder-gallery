bd32e2d6f2dbd9dcb1443b330ff9d40d8c7ef936
vjalby <vjalby@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-03 06:19:54 +0000

    tagging version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/folder-gallery/tags/1.0@662732 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- foldergallery.php
+++ foldergallery.php
@@ -1,12 +1,12 @@
 <?php
 /*
 Plugin Name: Folder Gallery
-Version: 0.96b
+Version: 1.0
 Plugin URI: http://www.jalby.org/wordpress/
 Author: Vincent Jalby
 Author URI: http://www.jalby.org
 Text Domain: foldergallery
-Description: This plugin creates picture galleries from a folder. The gallery is automatically generated in a post or page with a shortcode. Usage: [foldergallery folder="path_to_folder" title="Gallery title"]. For each gallery, a subfolder cache_[width]x[height] is created inside the pictures folder when the page is accessed for the first time. The picture folder must be writable (chmod 777).
+Description: This plugin creates picture galleries from a folder. The gallery is automatically generated in a post or page with a shortcode. Usage: [foldergallery folder="local_path_to_folder" title="Gallery title"]. For each gallery, a subfolder cache_[width]x[height] is created inside the pictures folder when the page is accessed for the first time. The picture folder must be writable (chmod 777).
 Tags: gallery, folder, lightbox, lightview
 Requires: 3.5
 License: GPLv2
@@ -38,11 +38,12 @@ class foldergallery{
 	var $lightview_path;
 
 	function foldergallery() {		
-		add_action( 'admin_menu', array( $this, 'fg_menu' ) );
+		add_action( 'admin_menu', array( $this, 'fg_menu' ) );	
 		add_action( 'admin_init', array( $this, 'fg_settings_init' ) );
 		add_action('plugins_loaded', array( $this, 'fg_init' ) );
 		add_action( 'wp_enqueue_scripts', array( $this, 'fg_styles' ) );
 		add_shortcode( 'foldergallery', array( $this, 'fg_gallery' ) );
+		add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), array( $this, 'fg_plugin_action_links' ) );
 	}
 
 	function fg_init() {
@@ -51,6 +52,10 @@ class foldergallery{
 		$fg_options = get_option( 'FolderGallery' );
 		if ( empty( $fg_options ) ) {
 			update_option( 'FolderGallery', $this->fg_settings_default() );
+		} elseif ( isset( $fg_options['images_per_row'] ) ) { // images_per_row => columns
+			$fg_options['columns'] = $fg_options['images_per_row'];
+			unset( $fg_options['images_per_row'] );
+			update_option( 'FolderGallery', $fg_options );
 		}
 		if ( ! $fg_options['engine'] ) {
 			$fg_options['engine'] = 'lightbox2';
@@ -68,14 +73,18 @@ class foldergallery{
 		$fg_options = get_option( 'FolderGallery' );
 		wp_enqueue_style( 'fg-style', plugins_url( '/css/style.css', __FILE__ ) );
 		switch ( $fg_options['engine'] ) {
+			case 'lightbox2' :
+				wp_enqueue_style( 'fg-lightbox-style', plugins_url( '/css/lightbox.css', __FILE__ ) );
+			break;
 			case 'fancybox2' :
 				wp_enqueue_style( 'fg-fancybox-style', plugins_url( '/fancybox/source/jquery.fancybox.css', __FILE__ ) );
 			break;
 			case 'lightview' :
 				wp_enqueue_style( 'lightview', plugins_url( '/lightview/css/lightview/lightview.css', __FILE__ ) );		
 			break;
-			default : // lightbox2
-				wp_enqueue_style( 'fg-lightbox-style', plugins_url( '/css/lightbox.css', __FILE__ ) );
+			case 'none' :
+				// do nothing for now
+			break;
 		}
 	}
 
@@ -83,6 +92,17 @@ class foldergallery{
 		static $firstcall = 1;
 		$fg_options = get_option( 'FolderGallery' );	
 		switch ( $fg_options['engine'] ) {
+			case 'lightbox2' :
+				wp_enqueue_script( 'fg-lightbox-script', plugins_url( '/js/fg_lightbox.js', __FILE__ ), array( 'jquery' ) );
+				if ( $firstcall ) {
+					wp_localize_script( 'fg-lightbox-script', 'FGtrans', array(
+						'labelImage' => __( 'Image', 'foldergallery' ),
+						'labelOf'    => __( 'of', 'foldergallery' ),
+						)
+					);
+					$firstcall = 0;
+				}
+			break;
 			case 'fancybox2' :
 				wp_enqueue_script( 'fancybox', plugins_url( '/fancybox/source/jquery.fancybox.pack.js', __FILE__ ), array( 'jquery' ) );
 				wp_enqueue_script( 'fg_fancybox', plugins_url( '/js/fg_fancybox.js', __FILE__ ), array( 'jquery' ) );
@@ -95,16 +115,9 @@ class foldergallery{
 				wp_enqueue_script( 'lightview_spinners', plugins_url( '/lightview/js/spinners/spinners.min.js', __FILE__ ), array( 'jquery' ) );
 				wp_enqueue_script( 'lightview', plugins_url( '/lightview/js/lightview/lightview.js', __FILE__ ) );   		
 			break;
-			default : // lightbox2
-				wp_enqueue_script( 'fg-lightbox-script', plugins_url( '/js/fg_lightbox.js', __FILE__ ), array( 'jquery' ) );
-				if ( $firstcall ) {
-					wp_localize_script( 'fg-lightbox-script', 'FGtrans', array(
-						'labelImage' => __( 'Image', 'foldergallery' ),
-						'labelOf'    => __( 'of', 'foldergallery' ),
-						)
-					);
-					$firstcall = 0;
-				}
+			case 'none' :
+				// Do nothing for now
+			break;
 		}		
 	}
 
@@ -145,7 +158,7 @@ class foldergallery{
 			'title'   => 'My Gallery',
 			'width'   => $fg_options['thumbnails_width'],
 			'height'  => $fg_options['thumbnails_height'],
-			'cols'    => $fg_options['images_per_row'],
+			'columns' => $fg_options['columns'],
 			'margin'  => $fg_options['margin'],
 			'padding' => $fg_options['padding'],
 			'border'  => $fg_options['border'],
@@ -173,44 +186,45 @@ class foldergallery{
 			return;
 		}
 	
-		$this->fg_scripts();
-		
-		
-		
-		$lightbox_id = md5( $folder );
-	
 		$pictures = $this->file_array( $folder );
 
 		$imgstyle = "margin:0px {$margin}px {$margin}px 0px;";
 		$imgstyle .= "padding:{$padding}px;";
 		$imgstyle .= "border-width:{$border}px;";
 
+		$this->fg_scripts();			
+		$lightbox_id = md5( $folder );
 		$gallery_code = '<div class="fg_gallery">';
 
 		$NoP = count( $pictures );
 		for ( $idx = 0 ; $idx < $NoP ; $idx++ ) {
-			$thumbnail = $cache_folder . '/' . $pictures[ $idx ];
+			$thumbnail = $cache_folder . '/' . strtolower($pictures[ $idx ]);
 			if ( ! file_exists( $thumbnail ) ) {
 				$this->save_thumbnail( $folder . '/' . $pictures[ $idx ], $thumbnail, $width, $height );
 			}
 			$gallery_code .= "\n";
 			switch ( $fg_options['engine'] ) {
+				case 'lightbox2' :
+					$gallery_code.= '<a title="' . $title . '" href="' . home_url( '/' ) . $folder . '/' . $pictures[ $idx ] . '" rel="lightbox[' . $lightbox_id . ']">';
+				break;
 				case 'fancybox2' :
-					$gallery_code.= '<a class ="fancybox" title="' . $title . '" href="' . home_url( '/' ) . $folder . '/' . $pictures[ $idx ] . '" rel="' . $lightbox_id . '">';
+					$subtitle = $title . ' (' . ($idx+1) . '/' . $NoP . ')';
+					$gallery_code.= '<a class ="fancybox" title="' . $subtitle . '" href="' . home_url( '/' ) . $folder . '/' . $pictures[ $idx ] . '" rel="' . $lightbox_id . '">';
 				break;
 				case 'lightview' :
 					if ( $options ) $options = " data-lightview-group-options=\"$options\"";
-					$gallery_code.= '<a title="' . $title . '" href="' . home_url( '/' ) . $folder . '/' . $pictures[ $idx ] . '" class="lightview" data-lightview-group="' . $lightbox_id . '"' . $options . '>';
+					$gallery_code .= '<a title="' . $title . '" href="' . home_url( '/' ) . $folder . '/' . $pictures[ $idx ] . '" class="lightview" data-lightview-group="' . $lightbox_id . '"' . $options . '>';
 					$options = ''; // group-options required only once per group.
 				break;
-				default : // lightbox2
-					$gallery_code.= '<a title="' . $title . '" href="' . home_url( '/' ) . $folder . '/' . $pictures[ $idx ] . '" rel="lightbox[' . $lightbox_id . ']">';
+				case 'none' :
+					$gallery_code .= '<a title="' . $title . '" href="' . home_url( '/' ) . $folder . '/' . $pictures[ $idx ] . '">';
+				break;
 			}		
 
-			$gallery_code.= '<img src="' . home_url( '/' ) . $thumbnail . '" style="' . $imgstyle . '" alt="' . $title . ' [' . ( $idx + 1 ) . ']' . '" /></a>';
+			$gallery_code .= '<img src="' . home_url( '/' ) . $thumbnail . '" style="' . $imgstyle . '" alt="' . $title . ' [' . ( $idx + 1 ) . ']' . '" /></a>';
 
-			if ( $cols>0 ) {
-				if ( ( $idx + 1 ) % $cols == 0 ) $gallery_code .= "\n" . '<div class="fg_clear"></div>';
+			if ( $columns>0 ) {
+				if ( ( $idx + 1 ) % $columns == 0 ) $gallery_code .= "\n" . '<br style="clear: both" />';
 			}
 		}
 		if ( 0 == $NoP ) {
@@ -219,22 +233,31 @@ class foldergallery{
 			echo '</p>';
 		}	
 		$gallery_code .= "\n</div>";
-		$gallery_code .= "\n" . '<div class="fg_clear"></div>';
+		$gallery_code .= "\n" . '<br style="clear: both" />';
 		return $gallery_code;
 	}
 
 	/* --------- Folder Gallery Settings --------- */
 
 	function fg_menu() {
-		add_options_page( 'Folder Gallery Settings', 'Folder Gallery', 'manage_options', 'foldergallery-settings', array( $this, 'fg_settings' ) );
+		add_options_page( 'Folder Gallery Settings', 'Folder Gallery', 'manage_options', 'folder-gallery', array( $this, 'fg_settings' ) );
 	}
 
 	function fg_settings_init() {
 		register_setting( 'FolderGallery', 'FolderGallery', array( $this, 'fg_settings_validate' ) );
 	}
 
+	function fg_plugin_action_links( $links ) { 
+ 		// Add a link to this plugin's settings page
+ 		$settings_link = '<a href="' . admin_url( 'options-general.php?page=folder-gallery' ) . '">' . __('Settings') . '</a>';
+ 		array_unshift( $links, $settings_link ); 
+ 		return $links; 
+	}
+	
+	
+
 	function fg_settings_validate( $input ) {
-		$input['images_per_row']    = intval( $input['images_per_row'] );
+		$input['columns']    = intval( $input['columns'] );
 		$input['thumbnails_width']  = intval( $input['thumbnails_width'] );
 			if ( 0 == $input['thumbnails_width'] ) $input['thumbnails_width'] = 150;
 		$input['thumbnails_height'] = intval( $input['thumbnails_height'] );
@@ -249,7 +272,7 @@ class foldergallery{
 				'border' 			=> 1,
 				'padding' 			=> 2,
 				'margin' 			=> 5,
-				'images_per_row' 	=> 0,
+				'columns' 	        => 0,
 				'thumbnails_width' 	=> 160,
 				'thumbnails_height' => 0,
 				'lw_options'        => '',
@@ -292,9 +315,19 @@ class foldergallery{
 			if ( 'lightview' == $fg_options['engine'] ) echo ' selected="selected"';
 			echo '>Lightview 3 (free for non commercial site)</option>' . "\n";
 		}
+		
+		echo "\t" .	'<option value="none"';
+		if ( 'none' == $fg_options['engine'] ) echo ' selected="selected"';
+		echo '>' . __( 'None', 'foldergallery') . '</option>' . "\n";
+
 		echo "</select>\n";
 
 		switch ( $fg_options['engine'] ) {
+			case 'lightbox2' :
+				echo '<p><a href="http://lokeshdhakar.com/projects/lightbox2/" target="_blank">Lightbox</a> is completely free to use. ';
+				echo 'If you are using Lightbox on a commercial project and feeling generous, consider a <a href="http://lokeshdhakar.com/projects/lightbox2/#donate" target="_blank">donation</a>. ';
+				echo 'All donations are sincerely appreciated. Thanks!</p>';
+			break;
 			case 'fancybox2' :
 				echo '<p><a href="http://fancyapps.com/fancybox/" target="_blank">Fancybox 2</a> is licensed under <a href="http://creativecommons.org/licenses/by-nc/3.0/" target="_blank">Creative Commons Attribution-NonCommercial 3.0 license</a>. ';
 				echo 'You are free to use fancyBox for your personal or non-profit website projects.<br />';
@@ -304,15 +337,10 @@ class foldergallery{
 				echo '<p><a href="http://projects.nickstakenburg.com/lightview" target="_blank">Lightview</a> is licensed under the terms of the <a href="http://projects.nickstakenburg.com/lightview/license" target="_blank">Lightview License</a>. ';
 				echo 'You are free to use it on non-commercial websites. Licenses are available for commercial use.</p>';
 			break;
-			case 'lightbox2' :
-				echo '<p><a href="http://lokeshdhakar.com/projects/lightbox2/" target="_blank">Lightbox</a> is completely free to use. ';
-				echo 'If you are using Lightbox on a commercial project and feeling generous, consider a <a href="http://lokeshdhakar.com/projects/lightbox2/#donate" target="_blank">donation</a>. ';
-				echo 'All donations are sincerely appreciated. Thanks!</p>';
-			break;
 		}
 		echo "</td>\n</tr>\n";
 
-		$this->fg_option_field( 'images_per_row', __( 'Images Per Row', 'foldergallery' ), __( '(0 = auto)', 'foldergallery' ) );
+		$this->fg_option_field( 'columns', __( 'Columns', 'foldergallery' ), __( '(0 = auto)', 'foldergallery' ) );
 		$this->fg_option_field( 'thumbnails_width', __( 'Thumbnails Width', 'foldergallery' ) );
 		$this->fg_option_field( 'thumbnails_height', __( 'Thumbnails Height', 'foldergallery' ), ' px ' . __( '(0 = auto)', 'foldergallery' ) );
 		$this->fg_option_field( 'border', __( 'Picture Border', 'foldergallery' ) );
